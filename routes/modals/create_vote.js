527360e85227012cdaae2b4ba9f3c054c3edbea6
qkrghdqls1 <qkrghdqls1@naver.com>
2021-04-29 15:45:29 +0000

    choice limit decreased

--- routes/modals/create_vote.js
+++ routes/modals/create_vote.js
@@ -38,66 +38,6 @@ create_vote_generator = () => {
 					{
 						text: '5',
 						value: '5'
-					},
-					{
-						text: '6',
-						value: '6'
-					},
-					{
-						text: '7',
-						value: '7'
-					},
-					{
-						text: '8',
-						value: '8'
-					},
-					{
-						text: '9',
-						value: '9'
-					},
-					{
-						text: '10',
-						value: '10'
-					},
-					{
-						text: '11',
-						value: '11'
-					},
-					{
-						text: '12',
-						value: '12'
-					},
-					{
-						text: '13',
-						value: '13'
-					},
-					{
-						text: '14',
-						value: '14'
-					},
-					{
-						text: '15',
-						value: '15'
-					},
-					{
-						text: '16',
-						value: '16'
-					},
-					{
-						text: '17',
-						value: '17'
-					},
-					{
-						text: '18',
-						value: '18'
-					},
-					{
-						text: '19',
-						value: '19'
-					},
-					{
-						text: '20',
-						value: '20'
 					}
 				],
 				required: true,
